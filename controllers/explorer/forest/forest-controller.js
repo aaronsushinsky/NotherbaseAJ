203d1376e3cb090d5cde070ef3b3a868fec02656
wsushinsky <wyattsushi@gmail.com>
2021-07-27 16:45:03 -0700

    added road and tower

--- controllers/explorer/forest/forest-controller.js
+++ controllers/explorer/forest/forest-controller.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 router.use("/clearing", require("./clearing/clearing-controller.js"));
 router.use("/the-front", require("./the-front/the-front-controller.js"));
+router.use("/road", require("./road/road-controller.js"));
+router.use("/pond", require("./pond/pond-controller.js"));
 
 router.use("/eye-of-the-forest", require("./eye-of-the-forest/eye-of-the-forest-controller.js"));
 
