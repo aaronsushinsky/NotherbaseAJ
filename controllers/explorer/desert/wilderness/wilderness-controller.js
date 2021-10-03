d017b800a9505cfbd5b5253f812703622282245a
wsushinsky <wyattsushi@gmail.com>
2021-10-03 00:44:46 -0700

    you can now get lost in the wilderness

--- controllers/explorer/desert/wilderness/wilderness-controller.js
+++ controllers/explorer/desert/wilderness/wilderness-controller.js
@@ -2,6 +2,7 @@
 const router = require("express").Router();
 
 // This is where all the routes for pois go.
+router.use("/edge", require("./edge/edge-controller.js"));
 router.use("/north", require("./north/north-controller.js"));
 router.use("/east", require("./east/east-controller.js"));
 router.use("/south", require("./south/south-controller.js"));
