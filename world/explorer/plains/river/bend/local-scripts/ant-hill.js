5953b2820a7a5c1385a68e625865092204f356dc
wsushinsky <wyattsushi@gmail.com>
2022-07-03 18:58:09 -0700

    ant hill deco

--- world/explorer/plains/river/bend/local-scripts/ant-hill.js
+++ world/explorer/plains/river/bend/local-scripts/ant-hill.js
@@ -3,6 +3,13 @@ class AntHill {
         this.$total = $(".ant-hill-game .total");
         this.$workerButtons = $(".ant-hill-game .workers button");
         this.bio = 10;
+        this.beatFrequency = 500;
+        this.$heart = $(".ant-hill-game #heart");
+        this.$males = $(".ant-hill-game #males-count");
+        this.$miners = $(".ant-hill-game #miners-count");
+        this.$warriors = $(".ant-hill-game #warriors-count");
+        this.$builders = $(".ant-hill-game #builders-count");
+        this.$females = $(".ant-hill-game #females-count");
         this.workers = {
             males: 0,
             miners: 0,
@@ -18,37 +25,69 @@ class AntHill {
             females: 1000
         }
 
-        this.update();
+        this.updateUI();
+
+        setInterval(this.beat, this.beatFrequency);
     }
 
-    update() {
+    beat = () => {
+        this.$heart.animate({
+            height: "0px",
+            opacity: 0
+        }, 3*this.beatFrequency/4, () => {
+            this.$heart.animate({
+                height: "10px",
+                opacity: 100
+            }, this.beatFrequency/4);
+        });
+    }
+
+    updateUI() {
         this.$total.text(`${this.bio} BIO`);
 
-        if (this.bio >= this.cost.males) this.$workerButtons[0].prop("disabled", false);
-        else this.$workerButtons[0].prop("disabled", true);
+        if (this.bio >= this.cost.males) $(this.$workerButtons[0]).prop("disabled", false);
+        else $(this.$workerButtons[0]).prop("disabled", true);
+
+        this.$males.text(`${this.workers.males} Males`);
     }
 
     birthMale() {
         if (this.bio >= 10) {
             this.workers.males++;
             this.bio -= 10;
-            this.update();
+            this.updateUI();
         }
     }
 
     birthMiner() {
-        
+        if (this.bio >= 100) {
+            this.workers.miners++;
+            this.bio -= 100;
+            this.updateUI();
+        }
     }
 
     birthWarrior() {
-        
+        if (this.bio >= 100) {
+            this.workers.warriors++;
+            this.bio -= 100;
+            this.updateUI();
+        }
     }
 
     birthBuilder() {
-        
+        if (this.bio >= 100) {
+            this.workers.builders++;
+            this.bio -= 100;
+            this.updateUI();
+        }
     }
 
     birthFemale() {
-        
+        if (this.bio >= 10000) {
+            this.workers.females++;
+            this.bio -= 10000;
+            this.updateUI();
+        }
     }
 }
