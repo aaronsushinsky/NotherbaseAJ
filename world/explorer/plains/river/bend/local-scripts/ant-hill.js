44fb5c5d47da6cac824ecbf9646056b7e07d2a7b
wsushinsky <wyattsushi@gmail.com>
2022-07-06 19:43:02 -0700

    ant hill gameplay

--- world/explorer/plains/river/bend/local-scripts/ant-hill.js
+++ world/explorer/plains/river/bend/local-scripts/ant-hill.js
@@ -1,93 +1,129 @@
+class Human {
+    constructor(health = 1) {
+        this.$window = $(".ant-hill-game .window");
+        this.$div = null;
+        this.health = health;
+
+        this.spawn();
+    }
+
+    spawn() {
+        this.$window.append(`<img src="/img/human.png" class="human"></img>`);
+        this.$div = this.$window.find(".human");
+    }
+
+    kill() {
+        this.$div.remove();
+        console.log("killed");
+    }
+}
+
 class AntHill {
     constructor() {
-        this.$total = $(".ant-hill-game .total");
+        this.$bio = $(".ant-hill-game .bio");
+        this.$ants = $(".ant-hill-game .ants");
         this.$workerButtons = $(".ant-hill-game .workers button");
         this.bio = 10;
+        this.bioCap = 1000;
         this.beatFrequency = 500;
         this.$heart = $(".ant-hill-game #heart");
-        this.$males = $(".ant-hill-game #males-count");
-        this.$miners = $(".ant-hill-game #miners-count");
-        this.$warriors = $(".ant-hill-game #warriors-count");
-        this.$builders = $(".ant-hill-game #builders-count");
-        this.$females = $(".ant-hill-game #females-count");
+        this.$workers = $(".ant-hill-game .workers p");
         this.workers = {
             males: 0,
             miners: 0,
             warriors: 0,
             builders: 0,
-            females: 0
+            females: 1
         };
+        this.workerCap = 10;
         this.cost = {
             males: 10,
             miners: 100,
             warriors: 1000,
             builders: 1000,
-            females: 1000
+            females: 100000
         }
+        this.humanSpawnCooldown = 60000;
+        this.human = null;
 
         this.updateUI();
 
         setInterval(this.beat, this.beatFrequency);
+        setInterval(this.spawnHuman, this.humanSpawnCooldown)
     }
 
     beat = () => {
         this.$heart.animate({
             height: "0px",
             opacity: 0
-        }, 3*this.beatFrequency/4, () => {
+        }, 3*this.beatFrequency / 4, () => {
             this.$heart.animate({
                 height: "10px",
                 opacity: 100
-            }, this.beatFrequency/4);
+            }, this.beatFrequency / 4);
         });
+
+        if (this.human) {
+            if (this.workers.warriors > 0) {
+                this.workers.warriors--;
+                this.human.kill();
+                this.human = null;
+            }
+            else if (this.getTotalAnts() > 0) {
+                let chosen = -1;
+                do {
+                    chosen = Math.floor(Math.random() * Object.keys(this.workers).length);
+                } while (Object.values(this.workers)[chosen] < 1);
+
+                this.workers[Object.keys(this.workers)[chosen]]--;
+            }
+            else {
+                this.human.kill();
+                this.human = null;
+            }
+        }
+
+        this.bio += this.workers.males * (this.beatFrequency / 1000) * 5;
+        if (this.bio > this.bioCap) this.bio = this.bioCap;
+
+        this.updateCaps();
+        this.updateUI();
+    }
+
+    getTotalAnts() {
+        return this.workers.males +  this.workers.miners + this.workers.warriors + this.workers.builders + this.workers.females;
     }
 
     updateUI() {
-        this.$total.text(`${this.bio} BIO`);
+        this.$bio.text(`${this.bio}/${this.bioCap} BIO`);
+        this.$ants.text(`${this.getTotalAnts()}/${this.workerCap} ANTS`);
 
-        if (this.bio >= this.cost.males) $(this.$workerButtons[0]).prop("disabled", false);
-        else $(this.$workerButtons[0]).prop("disabled", true);
-
-        this.$males.text(`${this.workers.males} Males`);
+        for (let i = 0; i < Object.values(this.workers).length; i++) {
+            if (this.bio >= Object.values(this.cost)[i] && this.getTotalAnts() < this.workerCap) $(this.$workerButtons[i]).prop("disabled", false);
+            else $(this.$workerButtons[i]).prop("disabled", true);
+    
+            $(this.$workers[i]).text(`${Object.values(this.workers)[i]} ${Object.keys(this.workers)[i]}`);
+        }
     }
 
-    birthMale() {
-        if (this.bio >= 10) {
-            this.workers.males++;
-            this.bio -= 10;
+    updateCaps() {
+        this.bioCap = (this.workers.builders + 1) * 1000;
+        this.workerCap = (this.workers.miners + 1) * 10;
+    }
+
+    birthWorker(which) {
+        if (this.bio >= this.cost[which] && this.getTotalAnts() < this.workerCap) {
+            this.workers[which]++;
+            this.bio -= this.cost[which];
+
+            this.updateCaps();
             this.updateUI();
         }
     }
 
-    birthMiner() {
-        if (this.bio >= 100) {
-            this.workers.miners++;
-            this.bio -= 100;
-            this.updateUI();
-        }
-    }
-
-    birthWarrior() {
-        if (this.bio >= 100) {
-            this.workers.warriors++;
-            this.bio -= 100;
-            this.updateUI();
-        }
-    }
-
-    birthBuilder() {
-        if (this.bio >= 100) {
-            this.workers.builders++;
-            this.bio -= 100;
-            this.updateUI();
-        }
-    }
-
-    birthFemale() {
-        if (this.bio >= 10000) {
-            this.workers.females++;
-            this.bio -= 10000;
-            this.updateUI();
+    spawnHuman = () => {
+        if (!this.human) {
+            this.human = new Human();
         }
     }
 }
