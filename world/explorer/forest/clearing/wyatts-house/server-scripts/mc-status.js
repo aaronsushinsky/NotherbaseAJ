78762eb4a935ed430efacb6736faacf0c0fb7bd9
wsushinsky <wyattsushi@gmail.com>
2022-07-21 14:57:43 -0700

    ip fix for mc

--- world/explorer/forest/clearing/wyatts-house/server-scripts/mc-status.js
+++ world/explorer/forest/clearing/wyatts-house/server-scripts/mc-status.js
@@ -2,7 +2,7 @@ module.exports = async function (db, currentRoute, user, req) {
     //73.11.150.171
     const pingBedrock = (...args) => import('@minescope/mineping').then(module => module.pingBedrock(...args));
 
-    const data = await pingBedrock('10.0.0.83', {
+    const data = await pingBedrock('73.11.150.171', {
         port: 19132,
         timeout: 500
     });
