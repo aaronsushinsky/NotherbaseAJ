8717a5e5cc060fce81ab8f4bc6143ca3a7e38f2c
wsushinsky <wyattsushi@gmail.com>
2023-03-17 00:52:48 -0700

    browser is now extendable; recipes need work

--- world/forest/eye-of-the-forest/library/notes/save-notes.js
+++ world/forest/eye-of-the-forest/library/notes/save-notes.js
@@ -1,13 +1,13 @@
 export default async (req, user) => {
     let spirit = await req.db.Spirit.recallOrCreate({
-        route: "/forest/eye-of-the-forest/library/recipes",
+        route: "/forest/eye-of-the-forest/library/notes",
         scope: "local",
         parent: user.id,
-        service: "recipes"
+        service: "notes"
     }, null, {
-        recipes: []
+        items: []
     });
 
-    spirit.memory.data.recipes = req.body.data.recipes;
+    spirit.memory.data.items = req.body.data.items;
     await spirit.commit();
 }
