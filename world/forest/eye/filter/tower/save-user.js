8a111ee1382ce2cbc423258263d0a12ca3df6f4c
Wyatt Sushinsky <wyattsushi@gmail.com>
2023-05-13 01:32:00 -0700

    nbfs 3.1 update

--- world/forest/eye/filter/tower/save-user.js
+++ world/forest/eye/filter/tower/save-user.js
@@ -1,9 +1,9 @@
 export default async (req, user) => {
     if (user.memory.data.authLevels.includes("Creator")) {
-        let spirit = await req.db.User.recallOne(req.body.data.email);
+        let spirit = await req.db.User.recallOne(req.body.email);
         
         if (spirit) {
-            spirit.memory.data.authLevels = req.body.data.authLevels;
+            spirit.memory.data.authLevels = req.body.authLevels;
             await spirit.commit();
 
             return spirit.memory.data;
--- world/forest/eye/inn/save-ads.js
+++ world/forest/eye/inn/save-ads.js
@@ -2,6 +2,6 @@ export default async (req, user) => {
     if (user.memory.data.authLevels.includes("eotf-inn-ads")) {
         let spirit = await req.db.Spirit.recallOne("eotf-inn-ads");
 
-        await spirit.commit({ items: req.body.data.items });
+        await spirit.commit({ items: req.body.items });
     }
 }
