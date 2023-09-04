e4b6bd6c81d907a802fe7384cbb20951e7e7dd41
wsushinsky <wyattsushi@gmail.com>
2023-09-03 23:05:17 -0700

    save projector security

--- world/forest/eye/church/sanctuary/save-projector.js
+++ world/forest/eye/church/sanctuary/save-projector.js
@@ -1,7 +1,9 @@
 export default async function (req, user) {
-    let spirit = await req.db.Spirit.recallOne(`${req.body.id}-projector`);
-
-    await spirit.commit({
-        url: req.body.url
-    });
+    if (user.memory.data.authLevels.includes("Creator")) {
+        let spirit = await req.db.Spirit.recallOne(`${req.body.id}-projector`);
+    
+        await spirit.commit({
+            url: req.body.url
+        });
+    }
 }
