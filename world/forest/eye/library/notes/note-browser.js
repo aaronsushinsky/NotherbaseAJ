f6b9fb26024a8b3fa044755675f5befe5898e899
wsushinsky <wyattsushi@gmail.com>
2023-09-13 20:40:15 -0700

    notes

--- world/forest/eye/library/notes/note-browser.js
+++ world/forest/eye/library/notes/note-browser.js
@@ -1,22 +1,39 @@
-class NoteBrowser extends Browser {
-    constructor(id) {
-        let fields = new NBField({
-            name: "note",
-            label: "Note: ",
-            placeholder: "No Note",
-            multiple: true
-        }, [
-            new NBField({
-                name: "title",
-                label: "Title: ",
-                placeholder: "No Title",
-            }, "string"),
-            new NBField({
-                name: "content",
-                label: "Content: ",
-                placeholder: "No Content",
-            }, "long-string")
-        ]);
-        super(id, fields, true, "save-notes"); 
+const notesBrowser = new Browser("notes");
+
+const notesSearch = new SearchBox("notes");
+
+const fields = new NBField({
+    name: "note",
+    label: "Note: ",
+    placeholder: "No Note",
+    multiple: true
+}, [
+    new NBField({
+        name: "title",
+        label: "Title: ",
+        placeholder: "No Title",
+    }, "string"),
+    new NBField({
+        name: "content",
+        label: "Content: ",
+        placeholder: "No Content",
+    }, "long-string")
+]);
+
+const metaNotes = new MetaBrowser("recipes", notesBrowser, fields, {
+    $origin: $(".meta.buttons#notes"),
+    label: "Notes Controls",
+    editable: true,
+    multiple: true,
+    searchBox: notesSearch,
+    toLoad: async () => {
+        let res = await base.load("notes");
+        return res;
+    },
+    toSave: async (item, which) => {
+        await base.do("save-note", {
+            item,
+            which
+        });
     }
-}
+});
