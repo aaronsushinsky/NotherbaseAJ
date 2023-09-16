21a1451ddb6f5bb4b551d8bb759983cb7b865efe
wsushinsky <wyattsushi@gmail.com>
2023-09-16 16:52:58 -0700

    more browser updates

--- world/forest/eye/town-hall/management/remove-member.js
+++ world/forest/eye/town-hall/management/remove-member.js
@@ -1,30 +1,33 @@
 export default async function(req, user) {
-    let spirit = await req.db.Spirit.recallOne("group", null, null, req.body.groupID);
+    if (user.id) {
+        let spirit = await req.db.Spirit.recallOne("group", null, null, req.body.groupID);
 
-    let leader = null;
-    let removal = null;
-    let which = null;
-    
-    for (let i = 0; i < spirit.memory.data.members.length; i++) {
-        if (spirit.memory.data.members[i].id == req.body.userID) {
-            removal = spirit.memory.data.members[i];
-            which = i;
+        let leader = null;
+        let removal = null;
+        let which = null;
+        
+        for (let i = 0; i < spirit.memory.data.members.length; i++) {
+            if (spirit.memory.data.members[i].id == req.body.userID) {
+                removal = spirit.memory.data.members[i];
+                which = i;
+            }
+            if (spirit.memory.data.members[i].id == user.id) leader = spirit.memory.data.members[i];
         }
-        if (spirit.memory.data.members[i].id == user.id) leader = spirit.memory.data.members[i];
-    }
 
-    if (leader.auth.includes("Leader")) {
-        if (removal.id == leader.id) return "self-error";
-        else {
+        if (leader.auth.includes("Leader")) {
+            if (removal.id == leader.id) return "self-error";
+            else {
+                spirit.memory.data.members.splice(which, 1);
+                await spirit.commit();
+                return "removed";
+            }
+        }
+        else if (removal.id == leader.id) {
             spirit.memory.data.members.splice(which, 1);
             await spirit.commit();
             return "removed";
         }
+        else return "auth-error";
     }
-    else if (removal.id == leader.id) {
-        spirit.memory.data.members.splice(which, 1);
-        await spirit.commit();
-        return "removed";
-    }
-    else return "auth-error";
+    else return "login-error";
 }
--- world/pages/portfolio/save-example.js
+++ world/pages/portfolio/save-example.js
@@ -1,11 +1,11 @@
 export default async (req, user) => {
-    let spirit = await req.db.Spirit.recallOne("portfolio-examples", user.id);
-    console.log(spirit.memory);
+    let spirit = await req.db.Spirit.recallOne("portfolio-examples");
 
     if (!Array.isArray(spirit.memory.data)) spirit.memory.data = [];
 
+    console.log(req.body.item, req.body.which);
     //new and edit
-    if (req.body.item) spirit.memory.data[req.body.which] = req.body.item;
+    if (!req.body.delete && req.body.item) spirit.memory.data[req.body.which] = req.body.item;
     //delete
     else if (req.body.which > -1 && req.body.which < spirit.memory.data.length) {
         spirit.memory.data.splice(req.body.which, 1);
